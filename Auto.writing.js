1dd8d0ca092449553729894f4b4af6d76da01b83
Edwin Brandon Lopez Martinez <k9999-f@hotmail.com>
2021-08-23 23:40:30 -0500

    Subiendo archivos iniciales

---
+++ Auto.writing.js
@@ -0,0 +1,39 @@
+/* EFECTO SACADO DE ESCUELA DIGITAL */
+/* Funcion que recibe un string */
+//let writing = str =>{
+	
+/* Split transforma el string que se recibe de arriba y lo convierte en un array vacio */
+//	let arrFromStr = str.split('');
+//	let i = 0;
+//	let printStr = setInterval(function(){
+//	document.body.innerHTML += arrFromStr[i];
+		
+//		i++;
+//			if (i === arrFromStr.length){
+//				clearInterval(printStr);
+//				document.body.style.color = 'orange'
+//			}
+
+/* El numero es para controlar el tiempo en que tarda en escribirse el texto */
+//	}, 90);
+//};
+//writing('Bienvenidos A Mi Pagina Web');
+
+// --------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------------
+// EFECTO SACADO DE SOYCODIGOFUENTE.COM - PROBADO Y CORRE EXITOSAMENTE
+// function escribir(){
+// 	document.getElementById('E').innerHTML = p.substr(0,num_c) + '...';
+// 	num_c++;
+// 		if (num_c <= p.length){
+// 			setTimeout('escribir()', 100);
+// 		}
+// }
+// function efectomaquina(){
+// 	p = 'JAVASCRIPT TE DA UN MUNDO DE POSIBILIDADES';
+// 	num_c = 0;
+// 	escribir();
+// }
+// setTimeout('efectomaquina()',100);
